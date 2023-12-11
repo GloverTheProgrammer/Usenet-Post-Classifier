af5617fa2ae019680991e2ae9c6bc13518934169
David <davidjrglover@gmail.com>
2023-12-10 22:47:00 -0500

    base

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -6,54 +6,67 @@ import nltk
 from nltk.corpus import stopwords
 import string
 
-@st.cache_resource
+
 def load_model():
     model_path = 'multinomial_nb_model.joblib'
     return load(model_path)
 
 
-@st.cache_data
+
 def load_feature_names():
     feature_names_df = pd.read_csv('feature_names.csv')
-    return feature_names_df['word'].tolist()
+    return feature_names_df['title'].tolist()
+
+
+def preprocess_text(text, feature_names):
+    # Tokenize and remove stopwords and punctuation
+    words = text.split()
+    # Filter out short words and stop words, then lowercase
+    words = [word.lower() for word in words if len(word) > 2 and word.lower() not in stop_words]
+    # Initialize feature vector based on top 2000 words
+    features = np.zeros(len(feature_names))
+    # Count occurrences of each word in the top 2000 words
+    word_counts = {word: words.count(word) for word in words}
+    for i, word in enumerate(feature_names):
+        features[i] = word_counts.get(word, 0)
+    return features.reshape(1, -1)
+
+
+
+def load_target_names():
+    return np.load('target_names.npy', allow_pickle=True)
 
 
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english')) | set(string.punctuation)
 
 
-def preprocess_text(text, feature_names):
-    words = text.split()
-    # Remove stopwords and non-feature words, count word occurrences
-    word_counts = {word: words.count(word) for word in set(words) if word in feature_names and word not in stop_words}
-    # Transform to feature vector
-    features = np.zeros(len(feature_names))
-    for i, word in enumerate(feature_names):
-        features[i] = word_counts.get(word, 0)
-    return features
-
 
 model = load_model()
 feature_names = load_feature_names()
+target_names = load_target_names()
 
 
-st.title("Text Classification Demo")
-
-user_input = st.text_area("Enter text to classify:")
+st.title("Newsgroup Classification App")
+user_input = st.text_area("This app uses a machine learning model to classify text. Enter your text below and press 'Classify")
+prediction_option = st.selectbox("How many predictions would you like to see?", ('Top Prediction', 'Top 3'))
 
 if st.button("Classify"):
     if user_input:
-        processed_input = preprocess_text(user_input, feature_names)
-        prediction = model.predict([processed_input])
-        st.write(f"Predicted Class: {prediction[0]}")
+        with st.spinner("Classifying..."):
+            processed_input = preprocess_text(user_input, feature_names) # Ensure this function is defined
+            probabilities = model.predict_proba(processed_input)[0]
+            top_indices = np.argsort(probabilities)[-3:][::-1]
+            top_classes = [target_names[i] for i in top_indices]
+            top_probabilities = probabilities[top_indices]
+
+            # Display predictions based on user choice
+            if prediction_option == 'Top Prediction':
+                st.write(f"Top Prediction: {top_classes[0]} with a probability of {top_probabilities[0]:.2f}")
+            else:
+                st.write("Top 3 Predictions:")
+                for i in range(3):
+                    st.write(f"{i+1}: {top_classes[i]} with a probability of {top_probabilities[i]:.2f}")
+        st.success("Classification Done!")
     else:
         st.error("Please enter some text to classify.")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as string:
-    string_data = uploaded_file.getvalue()
-
-    # or to read file into a dataframe (e.g., if it's a CSV):
-    import pandas as pd
-    df = pd.read_csv(uploaded_file)
